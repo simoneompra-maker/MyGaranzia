739fb9de2e4131d96eacf5b7007e7269be253929
simoneompra-maker <simone.ompra@gmail.com>
2026-05-08 11:18:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
-const CACHE = 'mygaranzia-v2';
+const CACHE = 'mygaranzia-v3';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/data/data.json'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
   if (e.request.url.includes('supabase.co')) return;
   e.respondWith(
